aa4a8e24de0f9d6d4e897e298cf7b6bf8ca1dc8b
12345aaw <willis.andrew.a@gmail.com>
2017-12-29 23:50:55 -0500

    code formatting

--- public/app.js
+++ public/app.js
@@ -1,56 +1,57 @@
 (function () {
 	angular
-		.module("BlogApp",[])
+		.module("BlogApp", [])
 		.controller("BlogController", BlogController);
 
-		function BlogController($scope, $http) {
-			$scope.createPost = createPost;
-			$scope.deletePost = deletePost;
-			$scope.editPost = editPost;
-			$scope.updatePost = updatePost;
+	function BlogController($scope, $http) {
+		$scope.createPost = createPost;
+		$scope.deletePost = deletePost;
+		$scope.editPost = editPost;
+		$scope.updatePost = updatePost;
 
-			function init(){
-				getAllPosts();
-			}
-			init();
+		function init() {
+			getAllPosts();
+		}
+		init();
 
-			function updatePost(post){
-				console.log(post);
-				$http
-					.put("/api/blogpost/"+post._id, post)
-					.success(getAllPosts);
-			}
+		function updatePost(post) {
+			console.log(post);
+			$http
+				.put("/api/blogpost/" + post._id, post)
+				.success(getAllPosts);
+		}
 
-			function editPost(postId){
-				$http
-					.get("/api/blogpost/"+postId)
-					.success(function(post){
-						$scope.post = post;
-					});
+		function editPost(postId) {
+			$http
+				.get("/api/blogpost/" + postId)
+				.success(function (post) {
+					$scope.post = post;
+				});
 
-			}
+		}
 
-			function deletePost(postId){
-				$http
-				.delete("/api/blogpost/"+postId)
+		function deletePost(postId) {
+			$http
+				.delete("/api/blogpost/" + postId)
 				.success(getAllPosts);
 
-			}
-			function getAllPosts(){
-				$http
-					.get("/api/blogpost")
-					.success(function(posts){
-						$scope.posts = posts;
-					});
-			}
-
-			function createPost(post) {
-				console.log(post);
-				console.log("good food");
-				$http
-					.post("/api/blogpost",post)
-					.success(getAllPosts);
-
-			}
 		}
+
+		function getAllPosts() {
+			$http
+				.get("/api/blogpost")
+				.success(function (posts) {
+					$scope.posts = posts;
+				});
+		}
+
+		function createPost(post) {
+			console.log(post);
+			console.log("good food");
+			$http
+				.post("/api/blogpost", post)
+				.success(getAllPosts);
+
+		}
+	}
 })();
--- server.js
+++ server.js
@@ -5,17 +5,30 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/blog2018');
 
 var PostSchema = mongoose.Schema({
-	title: {type: String, required:true},
+	title: {
+		type: String,
+		required: true
+	},
 	body: String,
-	tag: {type: String, enum: ['POLITICS', 'ECONOMY', 'EDUCATION']},
-	posted: {type: Date, default: Date.now}
-}, {collection: 'post'});
+	tag: {
+		type: String,
+		enum: ['POLITICS', 'ECONOMY', 'EDUCATION']
+	},
+	posted: {
+		type: Date,
+		default: Date.now
+	}
+}, {
+	collection: 'post'
+});
 
 var PostModel = mongoose.model("PostModel", PostSchema);
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
 
 app.post("/api/blogpost", createPost);
 app.get("/api/blogpost", getAllPosts);
@@ -23,64 +36,70 @@ app.delete("/api/blogpost/:id", deletePost);
 app.get("/api/blogpost/:id", getPostById);
 app.put("/api/blogpost/:id", updatePost);
 
-function updatePost(req,res){
+function updatePost(req, res) {
 	var postId = req.params.id;
 	var post = req.body;
 	PostModel
-		.update({_id: postId}, {
+		.update({
+			_id: postId
+		}, {
 			title: post.title,
 			body: post.body
 		})
 		.then(
-			function(status){
+			function (status) {
 				res.sendStatus(200);
 			},
-			function(err){
+			function (err) {
 				res.sendStatus(400);
 			});
 }
 
-function deletePost(req, res){
+function deletePost(req, res) {
 	var postId = req.params.id;
 	PostModel
-		.remove({_id: postId})
+		.remove({
+			_id: postId
+		})
 		.then(
-			function(status){
+			function (status) {
 				res.sendStatus(200);
 			},
-			function() {
+			function () {
 				res.sendStatus(400);
 			}
-			);
+		);
 
 
 }
 
-function getPostById(req, res){
+function getPostById(req, res) {
 	var postId = req.params.id;
 	PostModel
-		.findById({_id: postId})
+		.findById({
+			_id: postId
+		})
 		.then(
 			function (post) {
 				res.json(post);
 			},
-			function (err){
+			function (err) {
 				res.sendStatus(400);
 			}
-			);
+		);
 }
 
-function getAllPosts(req, res){
+function getAllPosts(req, res) {
 	PostModel
 		.find()
 		.then(
-				function (posts){
-					res.json(posts);
-				},
-				function (err) {
-					res.sendStatus(400);
-				}
-			);
+			function (posts) {
+				res.json(posts);
+			},
+			function (err) {
+				res.sendStatus(400);
+			}
+		);
 
 }
 
@@ -97,7 +116,7 @@ function createPost(req, res) {
 				res.sendStatus(400);
 			}
 		);
-	
+
 }
 
 app.listen(3000);
